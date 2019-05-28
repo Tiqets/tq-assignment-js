2b56ae4d5ff194cc77d3abc2bd993b7b567cc82d
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2019-05-28 21:54:46 +0200

    Add styles for the header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          className="App-logo"
+          src="https://www.tiqets.com/static/assets/webpack/logo/logo-white-medium.0c98d94780.png"
+          alt="Tiqets.com"
+        />
       </header>
     </div>
   );
